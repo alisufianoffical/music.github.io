c96f2fc2a4a0ad54aa487a30f16c0ebd059f544b
alisufianoffical <84099617+alisufianoffical@users.noreply.github.com>
2021-05-19 13:12:22 +0500

    Update script.js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -16,7 +16,7 @@ const songs = [
   {
     title: 'Ana Dmee',
     artist: 'Free Palistine',
-    coverPath: 'assets/images/falas.png',
+    coverPath: 'assets/images/falas.PNG',
     discPath: 'assets/music/music7.mp3',
     duration: '3:41',
   },
